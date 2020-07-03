b9783674d8dcb91d83160a9a9886e270c26c7e44
tylerkotler <54518126+tylerkotler@users.noreply.github.com>
2020-07-03 17:38:32 -0400

    Delete user_info.py

--- user_info.py
+++
@@ -1,5 +0,0 @@
-#Get a token_v2 from the Notion HA page
-#To do this go on notion and right click -> inspect -> application -> Storage -> Cookies
-#Find the token_v2 value, and copy and paste it in here:
-
-token_v2 = ""
