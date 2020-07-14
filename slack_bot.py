6119905c340774e6aa61436188574e0e20b24449
Tyler Kotler <tyler@humanagency.org>
2020-07-14 13:58:20 -0400

    Change threading

--- slack_bot.py
+++ slack_bot.py
@@ -46,10 +46,10 @@ def home():
 
 @app.route("/run-data", methods=['POST'])
 def run_data():
-    t = threading.Thread(target=notion_data.main())
-    t.setDaemon(False)
-    t.start()
-    print("returning to home")
+    return_home()
+    notion_data.main()
+    
+def return_home():
     return redirect('/')
 
 @app.route("/files", methods=['POST', 'GET'])
