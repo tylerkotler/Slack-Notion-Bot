9cead0ff7e1eb397b8a06ae565c2719fcedcc5f6
Tyler Kotler <tyler@humanagency.org>
2020-07-27 14:46:36 -0400

    Fix subcommand help

--- command_help.py
+++ command_help.py
@@ -34,14 +34,23 @@ def get_subcommand_help(documentation, page_link):
     for child in documentation.children:
         if 'Subcommands' in child.title:
             for subchild in child.children:
-                help_output = help_output + subchild.title
-                for child1 in subchild.children:
-                    help_output = help_output + "\n -- " + child1.title
-                    for child2 in child1.children:
-                        help_output = help_output + "\n   -- " + child2.title
-                        for child3 in child2.children:
-                            help_output = help_output + "\n     -- " + child3.title
-                help_output = help_output+"\n"
+                if 'Syntax' in subchild.title:
+                    syntax = subchild
+                    for item in syntax.children:
+                        help_output = help_output + item.title
+                        for example in item.children:
+                            help_output = help_output + "\n -- " + example.title
+                    help_output = help_output + "\n\nSubcommands:\n"
+                if 'Subcommands' in subchild.title:
+                    subcommands = subchild
+                    subcommand_count = 1
+                    for subcommand in subcommands.children:
+                        help_output = help_output + subcommand_count + ". " + subcommand.title
+                        for subcommand_child in subcommand.children:
+                            help_output = help_output + "\n -- " + subcommand_child.title
+                            for subcommand_child2 in subcommand_child.children:
+                                help_output = help_output + "\n    -- " + subcommand_child2.title
+                        help_output = help_output + "\n"
     help_output = help_output + f"\n\nFind full documentation on subcommands here: {page_link}"
     return help_output
 
