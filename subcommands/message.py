7bf7385383681ac124840d4f9a83739c73d09348
Tyler Kotler <tyler@humanagency.org>
2020-07-27 11:58:40 -0400

    Remove prints

--- subcommands/message.py
+++ subcommands/message.py
@@ -7,17 +7,14 @@ slack_client = WebClient(slack_token)
 
 #Returns the note text
 def main(text):
-    print(text)
     text_copy = text
     #replace all tags with the tag to send back to slack
     #IE: @tyler -> <@4625929>
     at_count = text.count("@")
-    print(at_count)
     for i in range(0, at_count):
         if "@" in text_copy:
             user_start = text_copy.index("@")
         else:
-            print("Error no @")
             break
         user_end = user_start+1
         end = False
@@ -33,19 +30,15 @@ def main(text):
             user = text[user_start:]
         else:
             user = text[user_start:user_end]
-        print(user)
         slack_users = slack_client.users_list()
         tag_string = ''
         for slack_user in slack_users["members"]:
             name = slack_user.get('name')
             if name == user.strip("@"):
-                print("match found")
                 slack_id = slack_user.get('id')
                 tag_string = f"<@{slack_id}>"
-        print(tag_string)
         text = text.replace(user, tag_string)
         tag_string = tag_string.replace("@", "$")
         text_copy = text_copy.replace(user, tag_string)
-        print(text)
 
     return {'output': text}
