a2366a239c0578a2d1a07f0e6c983e36c07cd302
Tyler Kotler <tyler@humanagency.org>
2020-07-27 14:05:43 -0400

    Command help

--- command_help.py
+++ command_help.py
@@ -4,7 +4,8 @@ from notion.client import NotionClient
 notion_client = NotionClient(token_v2=notion_token_v2)
 
 def main(command):
-    documentation = notion_client.get_block("https://www.notion.so/humanagency/Slack-Bot-Dwayne-42cf24713efb437b9feeb2195c64491e")
+    page_link = "https://www.notion.so/humanagency/Slack-Bot-Dwayne-42cf24713efb437b9feeb2195c64491e"
+    documentation = notion_client.get_block(page_link)
 
     help_output = ""
     for child in documentation.children:
@@ -14,6 +15,7 @@ def main(command):
                     summary = subchild
                     help_output = help_output + subchild.title
                     for summary_child in summary.children:
-                        help_output = help_output + "\n - " + summary_child.title
+                        help_output = help_output + "\n -- " + summary_child.title
+    help_output = help_output + f"\n\nFind full documentation on /{command} command here: {page_link}"
     help_output = help_output + f"\n\nFor information on subcommands, input /{command} subcommands"
     return help_output
